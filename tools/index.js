cd6a0344d1f69d8e33fc87d157d9fe63b665d515
Tim Logatsang <73504912+logatsang@users.noreply.github.com>
2024-01-13 15:15:08 -0500

    Verification tool page change

--- tools/index.js
+++ tools/index.js
@@ -1,6 +1,22 @@
 const buttonGroup = document.getElementById("char-buttons");
 const textField = document.getElementById("text-field");
-var segments, segmentCount, curIndex = 0;
+const pageDisplay = document.getElementById("page-display");
+const pageNumberDisplay = document.getElementById("page-number");
+
+const pageMin = 100, pageMax = 200;
+var segments, segmentCount, curIndex = 0, pageIndex = pageMin;
+
+function pageChange(value) {
+    pageIndex += value;
+    pageIndex = Math.max(Math.min(pageIndex, pageMax), pageMin);
+    pageUpdate();
+}
+
+function pageUpdate() {
+    const pageNumber = String(pageIndex);
+    pageNumberDisplay.innerHTML = pageNumber;
+    pageDisplay.src = `img/${pageNumber.padStart(4, '0')}.jpg`
+}
 
 function insertAtCursor(myField, myValue) {
     // https://stackoverflow.com/questions/11076975/how-to-insert-text-into-the-textarea-at-the-current-cursor-position
@@ -25,7 +41,6 @@ function insertAtCursor(myField, myValue) {
     
     textField.focus();
     textField.setSelectionRange(newIndex, newIndex);
-    
 }
 
 function writeChar(charIndex) {
@@ -52,12 +67,21 @@ function update() {
     var result = "";
 
     Array.from(currentSegment).forEach((value, index) => {
-        result += `<button type="button" class="btn btn-outline-secondary" onclick="writeChar(${index})">${value}</button>`;
+        result += `<button type="button" class="btn btn-primary" onclick="writeChar(${index})">${value}</button>`;
     })
     buttonGroup.innerHTML = result;
 }
 
 function initialize() {
+    pageChange(0);
+
+    document.addEventListener("keydown", (event) => {
+        if (textField === document.activeElement) return;
+
+        if (event.key == "ArrowLeft") pageChange(-1);
+        else if (event.key == "ArrowRight") pageChange(+1); 
+    });
+
     // Load text
     fetch("verify.txt")
     .then((res) => res.text())
